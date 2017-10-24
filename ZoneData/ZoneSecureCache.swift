2347dd8f9473e31ea31b5e03a88e2f967905c83c
Nosakhare Belvi <nosakharebelvi@gmail.com>
2017-10-24 20:15:12 +0100

    sample update

---
+++ ZoneData/ZoneSecureCache.swift
@@ -0,0 +1,13 @@
+//
+// Created by Nosa Beliv on 10/24/17.
+// Copyright (c) 2017 Appzone Group. All rights reserved.
+//
+
+import Foundation
+class ZoneSecureCache{
+
+    func interpolateMessage(message:String) -> String{
+
+        return "\(message) is interpolated"
+    }
+}
---
+++ ZoneDataTests/ZoneSecureCacheTests.swift
@@ -0,0 +1,30 @@
+//
+// Created by Nosa Beliv on 10/24/17.
+// Copyright (c) 2017 Appzone Group. All rights reserved.
+//
+//
+//import Foundation
+//import XCTest
+//@testable import ZoneData
+//
+//class ZoneSecureCacheTests: XCTestCase {
+//    let secure = ZoneSecureCache()
+//    override func setUp() {
+//        super.setUp()
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDown() {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//        super.tearDown()
+//    }
+//
+//    func testInterpolation(){
+//        let test = "Belvi"
+//        let expected = "Belvi is interpolated"
+//        let result = secure.interpolateMessage(message: test)
+//        XCTAssert(result == expected)
+//    }
+//
+//}
+
